130b994d3bb5cf6be955aa09ddb39ea85874767a
yasakuR3 <88955116+yasakuR3@users.noreply.github.com>
2024-07-31 18:56:49 +0900

    Create okurisochi1.m
    送り装置最初にコマンドウィンドウに以下のように入力することによって動作が可能になる。
    ser=[];
    ser=GP_two_axes_motor_drive(2,0,ser);
    
    (a, b, c) bが0のとき、y軸を選択する。y軸は地面に対して鉛直である。aがプラスだと重力と反対方向に動く。マイナスだとその逆である。
    
    bが1のとき、x軸を選択する。x軸は地面に対して水平である。aがプラスだと、風洞を向いて、左方向に動く。マイナスだとその逆である。

---
+++ okurisochi1.m
@@ -0,0 +1,43 @@
+clearvars
+close all
+daq.reset()
+
+s = daq.createSession('ni');
+addAnalogInputChannel(s,'Dev1','ai0','Voltage');
+
+s.Rate=20e3; % サンプリングレート 周波数
+s.DurationInSeconds = 1;%サンプリングタイム　ある一点における計測時間
+ 
+ nx=10;
+ dx=10;
+ ny=10;
+ dy=10;
+
+ser=[];
+ser=GP_two_axes_motor_drive(2*dy,0,ser);
+
+for j=1:ny
+    for i=1:nx
+      sy=sprintf('%02d',j);
+       sx=sprintf('%02d',i);
+        fn=['data_',sy,'_',sx,'_01.mat']
+
+        data = startForeground(s);
+
+         save(fn,'data');
+
+        if i<nx
+           ser=GP_two_axes_motor_drive(-dx,1,ser);
+        else
+             ser=GP_two_axes_motor_drive(dx*(nx-1),1,ser);
+
+         end
+ 
+     end
+     if j<ny
+         ser=GP_two_axes_motor_drive(-dy,0,ser);
+
+     else
+         ser=GP_two_axes_motor_drive(dy*(ny-3),0,ser);
+     end
+ end
