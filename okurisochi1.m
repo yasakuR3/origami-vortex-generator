f50e67fdcf02b5f9ce11bf2285dfc017f2976ac1
yasakuR3 <88955116+yasakuR3@users.noreply.github.com>
2024-10-05 22:24:46 +0900

    Update okurisochi1.m

--- okurisochi1.m
+++ okurisochi1.m
@@ -6,37 +6,35 @@ s = daq.createSession('ni');
 addAnalogInputChannel(s,'Dev1','ai0','Voltage');
 
 s.Rate=20e3; % サンプリングレート 周波数
-s.DurationInSeconds = 1;%サンプリングタイム　ある一点における計測時間 計測時間を長くすることでノイズの影響を少なくできる。
+s.DurationInSeconds = 10; % ある一点における計測時間[s]
  
- nx=10;
- dx=10;
- ny=10;
- dy=10;
+ nx=29; % x軸方向における測定個数
+ dx=5; % x軸方向における測定間隔[mm]
+ ny=13; % y軸方向における測定個数
+ dy=5; % y軸方向における測定間隔[mm]
 
 ser=[];
 
-for j=1:ny
-    for i=1:nx
-      sy=sprintf('%02d',j);
-       sx=sprintf('%02d',i);
-        fn=['data_',sy,'_',sx,'_01.mat']
+for j=0:(ny-1)
+    for i=0:(nx-1)
+      sx=sprintf('%02d',i), sy=sprintf('%02d',j);
+
+        fn=['data_',sx,'_',sy,'_06.mat']
 
         data = startForeground(s);
 
          save(fn,'data');
 
-        if i<nx
-           ser=GP_two_axes_motor_drive(-dx,1,ser);
+        if i<(nx-1)
+           ser=GP_two_axes_motor_drive(-dx,1,ser);%右向きに移動
         else
-             ser=GP_two_axes_motor_drive(dx*(nx-1),1,ser);
+           ser=GP_two_axes_motor_drive(dx*(nx-1),1,ser);%左向きに戻る
 
+           ser=GP_two_axes_motor_drive(dy, 0, ser);
          end
- 
      end
-     if j<ny
-         ser=GP_two_axes_motor_drive(-dy,0,ser);
+end
 
-     else
-         ser=GP_two_axes_motor_drive(dy*(ny-3),0,ser);
-     end
- end
+% ser=GP_two_axes_motor_drive(dx*(nx-1),1,ser);%左向きに戻る
+
+ser=GP_two_axes_motor_drive(-dy*ny, 0, ser);
